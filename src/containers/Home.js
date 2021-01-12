4f39480305d7729437ad4169417beedc3dd88e1d
ClaFiore <claborghini@gmail.com>
2021-01-12 10:17:55 -0500

    mapStateToProps and mapDispatchToProps in home component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import Main from './Main'
 import Footer from './Footer'
 import '../css/Homepage.css'
+import {connect} from 'react-redux'
 
 const Home = props => {
     return(
@@ -14,4 +15,17 @@ const Home = props => {
     )
 }
 
-export default Home
+
+
+const mapStateToProps = state => {
+    return {view: state.viewReducer.view}
+
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        view: ((view) => dispatch({type: 'view', payload: view}))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
