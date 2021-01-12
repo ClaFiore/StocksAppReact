9fa3bb58f4d3fc92107471356bd08f40cab42491
ClaFiore <claborghini@gmail.com>
2021-01-12 10:36:43 -0500

    mapDispatchToProps to send action to viewReducer in navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import '../css/Homepage.css'
+import {connect} from 'react-redux'
 
 
 const Navbar = props => {
+    console.log(props)
+    const handleMenu = value => {
+        props.changeView(value)
+    }
+
+
     return(
         <div id='navbarContainerDiv'>
-            <div class='menuNav'>Home</div>
-            <div class='menuNav' >My Portfolios</div>
-            <div class='menuNav' >My Watchlist</div>
-            <div class='menuNav' >News</div>
+            <div class='menuNav' onClick= {() => handleMenu('home')}>Home</div>
+            <div class='menuNav' onClick= {() => handleMenu('myPortfolios')}>My Portfolios</div>
+            <div class='menuNav' onClick= {() => handleMenu('myWatchlist')}>My Watchlist</div>
+            <div class='menuNav' onClick= {() => handleMenu('news')}>News</div>
             {true ? <div class='menuNav' >Signup / Login</div> : <div class='menuNav' >Logout</div>} 
         </div>
     )
 }
 
-export default Navbar
+const mapStateToProps = state => {
+    return {view: state.viewReducer.view}
+
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        changeView: ((value) => dispatch({type: 'changeView', payload: value}))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
--- src/reducers/viewReducer.js
+++ src/reducers/viewReducer.js
@@ -2,7 +2,7 @@ let initialState = {view: 'home'}
 
 let viewReducer = (state = initialState, action) => {
     switch(action.type){
-        case 'view':
+        case 'changeView':
         return {...state,
                 view: action.payload}
         default:
