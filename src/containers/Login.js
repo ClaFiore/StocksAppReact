af582aa289a95bf82bfca34f298802791ef311df
ClaFiore <claborghini@gmail.com>
2021-01-15 10:50:23 -0500

    login component

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,10 +3,13 @@ import React from 'react'
 const Login = props => {
     return(
         <div>
+            <h1>LOGIN</h1>
             <form>
                 <input type='text' placeholder='Email Address' />
                 <input type='password' placeholder='Enter your password'/>
+                <input type='submit'/>
             </form>
+            <h3>Don't have an account yet?</h3><span>Signup now</span>
         </div>
     )
 }
