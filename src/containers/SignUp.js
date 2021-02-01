acc4a96417f85664205e24f8dc870b37dd7144a8
ClaFiore <claborghini@gmail.com>
2021-02-01 12:25:15 -0500

    submit function for signp

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -7,13 +7,26 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 const SignUp = props => {
 
     const [show, setShow] = React.useState(false);
+    const [first, setFirst] = React.useState('')
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+  
   
     const submitForm = () => {
-        console.log('clicked')
+        console.log('hi')
         handleClose()
+        let configObj = {
+            method: 'POST',
+            headers:{
+            'content-type': 'application/json',
+            Accept: 'application/json'
+            },
+        body: JSON.stringify({
+            
+        })
+        }
         //controlled form, save answers in state and use it to send a post request to backend to save new user info - and change state to be logged in
     }
 
@@ -30,17 +43,18 @@ const SignUp = props => {
           <Modal.Body>
               <Form>
                 <Form.Label>First Name</Form.Label>
-                <Form.Control name="firstname" placeholder="First name"/>
+                <Form.Control name="firstname" placeholder="First name" onChange={(e) => setFirst(e.target.value)}/>
                 <Form.Label>Last Name</Form.Label>
                 <Form.Control name="lastname" placeholder="Last name"/>
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control name="email" placeholder="Enter email address" />
                 <Form.Label>Password</Form.Label>
                 <Form.Control name="password" placeholder="Choose new password" type='password'/>
+                <Form.Control type='submit' onClick={()=>submitForm()}/>
               </Form>
           </Modal.Body>
           <Modal.Footer>
-          <Button type='submit' onClick={()=>submitForm()}>Sign Up</Button>
+          
           </Modal.Footer>
         </Modal>
       </div>
